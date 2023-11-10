3315716758f2b7d3e0d000f16942ce5410a39ba4
Y.Hisaki <yhisaki31@gmail.com>
2023-11-11 00:58:39 +0900

    feat: add goal_pose_setter

---
+++ docker/aichallenge/aichallenge_ws/src/aichallenge_submit/goal_pose_setter/src/goal_pose_setter_node.cpp
@@ -0,0 +1,73 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "autoware_adapi_v1_msgs/msg/route_state.hpp"
+
+class GoalPosePublisher : public rclcpp::Node
+{
+    bool stop_streaming_goal_pose_ = false;
+
+public:
+    GoalPosePublisher() : Node("goal_pose_publisher")
+    {
+        auto qos = rclcpp::QoS(rclcpp::KeepLast(10)).reliable();
+        goal_publisher_ = this->create_publisher<geometry_msgs::msg::PoseStamped>("/planning/mission_planning/goal", qos);
+        route_state_subscriber_ = this->create_subscription<autoware_adapi_v1_msgs::msg::RouteState>(
+            "/planning/mission_planning/route_state",
+            rclcpp::QoS(rclcpp::KeepLast(10)).reliable().transient_local(),
+            std::bind(&GoalPosePublisher::route_state_callback, this, std::placeholders::_1));
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(300),
+            std::bind(&GoalPosePublisher::publish_goal_pose, this));
+
+        this->declare_parameter("goal.position.x", 21928.861328125);
+        this->declare_parameter("goal.position.y", 51829.57421875);
+        this->declare_parameter("goal.position.z", 0.0);
+        this->declare_parameter("goal.orientation.x", 0.0);
+        this->declare_parameter("goal.orientation.y", 0.0);
+        this->declare_parameter("goal.orientation.z", 0.6982381759416851);
+        this->declare_parameter("goal.orientation.w", 0.7158655248422209);
+    }
+
+private:
+    void publish_goal_pose()
+    {
+        if (!stop_streaming_goal_pose_)
+        {
+            auto msg = std::make_shared<geometry_msgs::msg::PoseStamped>();
+            msg->header.stamp = this->get_clock()->now();
+            msg->header.frame_id = "map";
+            msg->pose.position.x = this->get_parameter("goal.position.x").as_double();
+            msg->pose.position.y = this->get_parameter("goal.position.y").as_double();
+            msg->pose.position.z = this->get_parameter("goal.position.z").as_double();
+            msg->pose.orientation.x = this->get_parameter("goal.orientation.x").as_double();
+            msg->pose.orientation.y = this->get_parameter("goal.orientation.y").as_double();
+            msg->pose.orientation.z = this->get_parameter("goal.orientation.z").as_double();
+            msg->pose.orientation.w = this->get_parameter("goal.orientation.w").as_double();
+
+            goal_publisher_->publish(*msg);
+            RCLCPP_INFO(this->get_logger(), "Publishing goal pose");
+        }
+    }
+
+    void route_state_callback(const autoware_adapi_v1_msgs::msg::RouteState::SharedPtr msg)
+    {
+        if (msg->state >= autoware_adapi_v1_msgs::msg::RouteState::SET)
+        {
+            stop_streaming_goal_pose_ = true;
+            RCLCPP_INFO(this->get_logger(), "Stop streaming goal pose");
+        }
+    }
+
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr goal_publisher_;
+    // subscribe route state
+    rclcpp::Subscription<autoware_adapi_v1_msgs::msg::RouteState>::SharedPtr route_state_subscriber_;
+    rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char const *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<GoalPosePublisher>());
+    rclcpp::shutdown();
+    return 0;
+}
