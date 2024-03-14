bd8b1cc868c302d7de1a289fa9486b8dd359ccb2
taikitanaka3 <taiki.tanaka@tier4.jp>
2024-03-14 12:20:46 +0900

    chore: add output as out dir

--- docker-evaluation/run_container.sh
+++ docker-evaluation/run_container.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+mkdir -p output
 rocker --nvidia --x11 --user --net host --privileged --volume output:/output -- aichallenge-202407-eval
--- docker-evaluation/run_container_cpu.sh
+++ docker-evaluation/run_container_cpu.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir -p output
 rocker --nvidia --x11 --user --net host --privileged --volume output:/output -- aichallenge-202407-eval
 
